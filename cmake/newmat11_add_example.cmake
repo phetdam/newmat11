021f4d017ad930b86a33752456f15161f85af215
Derek Huang <djh458@stern.nyu.edu>
2025-04-15 01:15:24 -0400

    add CMakeLists.txt for newmat11 + split out newmat11_add_example
    modified:   CMakeLists.txt
        Remove newmat11_add_example and move all library + examples to newmat11
    
    new file:   cmake/newmat11_add_example.cmake
        Add newmat11_add_example in separate CMake module
    
    new file:   newmat11/CMakeLists.txt
        Add CMake config with the newmat targets + original examples + test

---
+++ cmake/newmat11_add_example.cmake
@@ -0,0 +1,46 @@
+cmake_minimum_required(VERSION 3.15)
+
+##
+# newmat11_add_example.cmake
+#
+# Author: Derek Huang
+# License: MIT License
+#
+# Provides a function to simplify adding a single-source newmat11 example.
+#
+
+##
+# Helper function for registering a newmat example program.
+#
+# This builds a single ${target}.cpp file into an example program, links
+# against the newmat library target, and registers a CTest test However, if
+# NEWMAT11_BUILD_EXAMPLES is FALSE, a skip message is printed instead.
+#
+# The working directory of each test is the top-level source directory.
+#
+# Arguments:
+#   target                  Program target name
+#   [LIBRARIES libs...]     Additional libraries to link against
+#
+function(newmat11_add_example target)
+    # not building examples
+    if(NOT NEWMAT11_BUILD_EXAMPLES)
+        message(STATUS "Skipping ${PROJECT_NAME} example ${target}")
+        return()
+    endif()
+    # otherwise, parse libraries and add program
+    cmake_parse_arguments(ARG "" "" "LIBRARIES" ${ARGN})
+    add_executable(${target} ${target}.cpp)
+    target_link_libraries(${target} PRIVATE newmat)
+    if(ARG_LIBRARIES)
+        target_link_libraries(${target} PRIVATE ${ARG_LIBRARIES})
+    endif()
+    # register CTest test
+    add_test(
+        NAME ${target}
+        COMMAND ${target}
+        # ensures that any data files in the same directory as the program can
+        # be read without any additional changes to working directory
+        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+    )
+endfunction()
